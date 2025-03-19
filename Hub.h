d303ed9b349901fe00227f7a77ae8ec16bb43d51
furqan <furmak331@gmail.com>
2025-03-19 08:01:34 +0530

    modify Hub module for logfile

--- Hub.h
+++ Hub.h
@@ -2,13 +2,19 @@
 
 #include "Device.h"
 #include <map>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 // Represents a network hub that broadcasts data
 class Hub : public Device {
 private:
-    int nextPort;  // Next available port number
+    int nextPort;          // Next available port number
+    string logFilePath;    // Path to the log file
+    
+    // Helper method to log messages to file
+    void logMessage(const string& message);
 
 public:
     // Constructor
@@ -18,8 +24,11 @@ public:
     int connectDevice(Device* device);
     
     // Handle incoming data
-    void receiveData(const string& data, int incomingPort) override;
+    void receiveData(const string& data, int incomingPort, const string& senderName = "") override;
     
     // Hubs don't send data, only forward it
     void sendData(const string& data) override;
+    
+    // Get log file path
+    string getLogFilePath() const;
 };
